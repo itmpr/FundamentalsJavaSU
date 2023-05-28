fc4f8f4583fa025c3b1229ac7a826e9256c04cf3
Maria Marinova <60257918+itmpr@users.noreply.github.com>
2023-05-28 09:36:56 +0300

    Add files via upload

--- DataTypeExcersices/P09SpiceMustFlow.java
+++ DataTypeExcersices/P09SpiceMustFlow.java
@@ -1,5 +1,28 @@
 package DataTypeExcersices;
 
+import java.util.Scanner;
+
 public class P09SpiceMustFlow {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int startYeild = Integer.parseInt(scanner.nextLine());
+        int totalAmount = startYeild;
+        int days = 0;
+        int dayAmount = 0;
+        while (startYeild >= 100) {
+            dayAmount += startYeild - 26;
+            startYeild -= 10;
+            days ++;
+        }
+        if (startYeild < 100 && days == 0){
+            System.out.println(days);
+            System.out.println(dayAmount);
+        } else {
+            dayAmount -= 26;
+            System.out.println(days);
+            System.out.println(dayAmount);
+        }
+    }
 
 }
